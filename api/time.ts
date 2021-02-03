bbcc964c93316af7a0bd217a6b4ec73a98764a77
SolProc <info@cheeseswap.app>
2021-02-03 09:52:36 +0600

    clean

--- api/time.ts
+++
@@ -1,7 +0,0 @@
-import { NowRequest, NowResponse } from "@vercel/node";
-
-export default (_req: NowRequest, res: NowResponse) => {
-  const time = new Date().getTime();
-
-  res.json({ time });
-};
--- utils/mathUtils.ts
+++
@@ -1,7 +0,0 @@
-export const ceilDecimal = (value: number, decimal = 0): number => {
-  let multiplier = 1;
-  if (decimal > 0) {
-    multiplier = Math.pow(10, decimal);
-  }
-  return Math.ceil(value * multiplier) / multiplier;
-};
